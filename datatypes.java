a8b3235ede3cd84e30a59679c75e0daff7236959
sindrella008 <sindrellasindrella37@gmail.com>
2026-08-11 21:54:05 +0530

    Add datatypes class with main method

---
+++ datatypes.java
@@ -0,0 +1,18 @@
+public class datatypes {
+    public static void main(String[]args)
+{
+    int age=18;
+    float height=5.7f;
+    double pi=3.145678;
+    char grade='A';
+    boolean isStudent=true;
+    String name="sindrella";
+
+    System.out.println(age);
+    System.out.println(height);
+    System.out.println(pi);
+    System.out.println(grade);
+    System.out.println(isStudent);
+    System.out.println(name);
+}    
+}
