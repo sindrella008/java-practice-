25a56cfd15c0935541da160f94fa2b009c7d153f
sindrella008 <sindrellasindrella37@gmail.com>
2026-08-12 23:20:01 +0530

    Add print and println examples in main class

---
+++ print()andprintln().java
@@ -0,0 +1,12 @@
+public class main {
+    public static void main(String[] args) {
+
+        System.out.print("Hello ");
+        System.out.println("Java");
+
+        System.out.print("My name is ");
+        System.out.println("Sindrella");
+
+    }
+}
+    
