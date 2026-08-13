7181588e11fcdefe430ad8f038d252381657cf76
sindrella008 <sindrellasindrella37@gmail.com>
2026-08-13 22:05:00 +0530

    Create singlelinecommentpractice4.java

---
+++ singlelinecommentpractice4.java
@@ -0,0 +1,15 @@
+public class singlelinepractice4 {
+    
+    public static void main(String[] args) {
+
+        String name = "Sindrella";
+        int age = 18;
+
+        // This line prints the name
+        System.out.println(name);
+
+        // This line prints the age
+        System.out.println(age);
+
+    }
+}
