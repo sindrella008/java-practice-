5310046049b3468e26f10813485329ee5c94b8dc
sindrella008 <sindrellasindrella37@gmail.com>
2026-08-13 22:06:48 +0530

    Create singlelinecommentpractice3.java

---
+++ singlelinecommentpractice3.java
@@ -0,0 +1,11 @@
+public class singlelinecommentpractice3 {
+    
+    public static void main(String[] args) {
+
+        // Adding two numbers
+        System.out.println("10 + 20 = 30");
+
+    }
+}
+    
+
