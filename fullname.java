7fd4031da7db2dd0235a765b1e704961a06e7442
sindrella008 <sindrellasindrella37@gmail.com>
2026-08-13 22:34:50 +0530

    Create fullname.java

---
+++ fullname.java
@@ -0,0 +1,11 @@
+public class fullname {
+   
+    public static void main(String[] args) {
+        String firstName = "Sindrella";
+        String lastName = "Iyappan";
+
+        System.out.println(firstName + " " + lastName);
+    }
+}
+    
+
