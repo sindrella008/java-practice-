035a2e06460846b589f3efdc8e5b411f702bc274
sindrella008 <sindrellasindrella37@gmail.com>
2026-08-13 22:42:40 +0530

    Create floataverage.java

---
+++ floataverage.java
@@ -0,0 +1,12 @@
+public class floataverage {
+    
+    public static void main(String[] args) {
+        float a = 80.5f;
+        float b = 90.5f;
+
+        float average = (a + b) / 2;
+        System.out.println(average);
+    }
+}
+    
+
