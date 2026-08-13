2bb340540f621d00fbbc923a2dd2d768133a7fdd
sindrella008 <sindrellasindrella37@gmail.com>
2026-08-13 22:09:53 +0530

    Create multilinecomment.java

---
+++ multilinecomment.java
@@ -0,0 +1,15 @@
+public class multilinecommentpractice {
+    
+    public static void main(String[] args) {
+
+        /*
+         I am learning Java
+         Java is easy
+         I will practice daily
+        */
+
+        System.out.println("Hello Java");
+
+    }
+}
+    
