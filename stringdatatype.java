0159db1c78c1c75cc6be24db8b485a21e5a6c700
sindrella008 <sindrellasindrella37@gmail.com>
2026-08-13 23:07:56 +0530

    Add stringstudentdetails class with main method

---
+++ stringdatatype.java
@@ -0,0 +1,13 @@
+public class stringstudentdetails {
+    
+    public static void main(String[] args) {
+        String name = "Sindrella";
+        String college = "Sri Shanmugha College";
+        String department = "CSE";
+
+        System.out.println(name);
+        System.out.println(college);
+        System.out.println(department);
+    }
+}
+    
