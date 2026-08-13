ab61b54de2f75a64c443c9f9723f50bd02e3d986
sindrella008 <sindrellasindrella37@gmail.com>
2026-08-13 21:57:56 +0530

    Add singlelinecommentpractice2.java with student info

---
+++ singlelinecommentpractice2.java
@@ -0,0 +1,13 @@
+public class singlelinecommentpractice2 {
+    
+    public static void main(String[] args) {
+
+        // Printing student name
+        System.out.println("Name: Sindrella");
+
+        // Printing student age
+        System.out.println("Age: 18");
+
+    }
+}
+    
