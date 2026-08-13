f47d41bb8513412117d8230d95b597931aef3926
sindrella008 <sindrellasindrella37@gmail.com>
2026-08-13 22:47:26 +0530

    Add doubleaddition class to sum two doubles

---
+++ doubleaddition.java
@@ -0,0 +1,11 @@
+public class doubleaddition {
+    
+    public static void main(String[] args) {
+        double a = 10.5;
+        double b = 20.5;
+
+        System.out.println(a + b);
+    }
+}
+    
+
