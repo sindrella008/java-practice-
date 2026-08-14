400b78f8ed3646bfce03188bd4743addce55d94c
sindrella008 <sindrellasindrella37@gmail.com>
2026-08-14 23:07:10 +0530

    Add intdoubleaddnumbers class to sum int and double

---
+++ intdoubleaddnumbers.java
@@ -0,0 +1,12 @@
+public class intdoubleaddnumbers {
+    
+    public static void main(String[] args) {
+        int a = 10;
+        double b = 5.5;
+
+        double result = a + b;
+
+        System.out.println(result);
+    }
+
+}
