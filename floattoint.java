031b15fbab774698ad9569b930f3b0ea6bbc2358
sindrella008 <sindrellasindrella37@gmail.com>
2026-08-14 23:05:26 +0530

    Add float to int conversion example

---
+++ floattoint.java
@@ -0,0 +1,10 @@
+public class floattoint {
+    
+    public static void main(String[] args) {
+        float marks = 85.5f;
+        int result = (int) marks;
+
+        System.out.println(result);
+    }
+
+}
