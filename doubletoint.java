6f9ce313e69e18845c3843e63716bf83309e1679
sindrella008 <sindrellasindrella37@gmail.com>
2026-08-14 23:03:46 +0530

    Create doubletoint.java

---
+++ doubletoint.java
@@ -0,0 +1,9 @@
+public class doubletoint {
+    public static void main(String[]args){
+        double price=99.99;
+        int result=(int)price;
+        System.out.println(result);
+
+    }
+    
+}
