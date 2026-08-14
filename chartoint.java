a51df02bbe99df49a18f89d84eaa5424b1104005
sindrella008 <sindrellasindrella37@gmail.com>
2026-08-14 23:08:54 +0530

    Add chartoint.java to convert char to int

---
+++ chartoint.java
@@ -0,0 +1,10 @@
+public class chartoint {
+    
+    public static void main(String[] args) {
+        char ch = 'A';
+        int result = (int) ch;
+
+        System.out.println(result);
+    }
+}
+    
