5bf76eba496f0f0d8ee1351bbb74bf611912d2b6
sindrella008 <sindrellasindrella37@gmail.com>
2026-08-15 22:03:33 +0530

    Add name input functionality in nameinput.java

---
+++ nameinput.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class nameinput{
+    public static void main(String[]args){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter your name:");
+        String name=sc.nextLine();
+        System.out.println("hello, "+name);
+
+
+    }
+}
+
