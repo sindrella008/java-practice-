05a43d3613025eed2dd34351f9337dc449c9ef8a
sindrella008 <sindrellasindrella37@gmail.com>
2026-08-15 21:28:20 +0530

    Create circlearea.java

---
+++ circlearea.java
@@ -0,0 +1,9 @@
+public class circlearea {
+    public static void main(String[]args){
+        final double PI=3.1415516;
+        int radius=5;
+        double area=PI*radius*radius;
+        System.out.println(area);
+    }
+    
+}
