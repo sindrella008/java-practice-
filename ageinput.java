4619cc022c235261b5f062a253c843a7cef156c4
sindrella008 <sindrellasindrella37@gmail.com>
2026-08-15 21:48:43 +0530

    Add age input program in ageinput.java

---
+++ ageinput.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class ageinput{
+    public static void main(String[]args){
+        Scanner sc =new Scanner(System.in);
+        System.out.println("enter your age:");
+        int age= sc.nextInt();
+        System.out.println(age);
+    }
+}
