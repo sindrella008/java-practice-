a0ebed29fc5c5c6c9c4e7ea2fd8884b89ee5f962
sindrella008 <sindrellasindrella37@gmail.com>
2026-08-16 13:41:54 +0530

    Add basic Java program to display name and subject

---
+++ learningjava.java
@@ -0,0 +1,11 @@
+public class learningjava {
+    public static void main(String[] args) {
+        String name = "Sindrella";
+        String subject = "Java";
+
+        System.out.println("My name is " + name);
+        System.out.println("I am learning " + subject);
+        System.out.println("Java is interesting");
+    }
+}
+    
