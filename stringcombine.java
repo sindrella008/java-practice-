e1cb8bd8487841609f334da4b45b9d46c2e6e3ea
sindrella008 <sindrellasindrella37@gmail.com>
2026-08-16 13:40:36 +0530

    Add stringcombine class to concatenate strings

---
+++ stringcombine.java
@@ -0,0 +1,10 @@
+public class stringcombine {
+    public static void main(String[] args) {
+        String a = "Java";
+        String b = "Programming";
+
+        System.out.println(a + " " + b);
+    }
+}
+    
+
