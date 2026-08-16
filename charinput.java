c9f83bbe6d24ed6ae5d0117c2be9380b11710315
sindrella008 <sindrellasindrella37@gmail.com>
2026-08-16 13:27:52 +0530

    Add charinput.java to read and display a character

---
+++ charinput.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+    public class charinput{
+        public static void main(String[]args){
+            Scanner sc=new Scanner(System.in);
+            System.out.println("enter a character");
+            char ch=sc.next().charAt(0);
+            System.out.println(ch);
+        }
+    }
