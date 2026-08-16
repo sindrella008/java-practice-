ada8ce6c796905bd81a7e77e0c2cf380a7be02d8
sindrella008 <sindrellasindrella37@gmail.com>
2026-08-16 13:37:06 +0530

    Create details.java

---
+++ details.java
@@ -0,0 +1,7 @@
+public class Details {
+    public static void main(String[] args) {
+        System.out.println("Name: Sindrella");
+        System.out.println("Age: 18");
+        System.out.println("Department: CSE");
+    }
+}
