a2987951867a45bc8edb9d0e8f4285bbde3f09b9
sindrella008 <sindrellasindrella37@gmail.com>
2026-08-19 22:33:41 +0530

    Create assignmentoperatorpractice4.java

---
+++ assignmentoperatorpractice4.java
@@ -0,0 +1,10 @@
+public class assignmentoperatorpractice4 {
+    
+    public static void main(String[] args) {
+        int a = 6;
+        a *= 4;
+        System.out.println(a);
+    }
+}
+    
+
