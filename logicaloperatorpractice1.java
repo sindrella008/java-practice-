9a38ac92cfc0c9843987b53bea315f13de68fd45
sindrella008 <sindrellasindrella37@gmail.com>
2026-08-19 21:42:41 +0530

    Add logical operator practice Java program

---
+++ logicaloperatorpractice1.java
@@ -0,0 +1,9 @@
+public class logicaloperatorpractice1{
+    
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+
+        System.out.println(a < b && a > 5);
+    }
+}
