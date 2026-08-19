67d7c3cc363479f766efbc80cc1c506700fd7c40
sindrella008 <sindrellasindrella37@gmail.com>
2026-08-19 22:26:04 +0530

    Create assignmentoperatorpractice2.java

---
+++ assignmentoperatorpractice2.java
@@ -0,0 +1,10 @@
+public class assignmentoperatorpractice2 {
+    
+    public static void main(String[] args) {
+        int a = 10;
+        a += 5;
+        System.out.println(a);
+    }
+}
+    
+
