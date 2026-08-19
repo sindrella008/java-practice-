e1b99023252e070daeaf3ea49cb4d5b12e852472
sindrella008 <sindrellasindrella37@gmail.com>
2026-08-19 22:14:56 +0530

    Create logicaloperatorpractice7.java

---
+++ logicaloperatorpractice7.java
@@ -0,0 +1,11 @@
+public class logicaloperatorpractice7 {
+   
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+
+        System.out.println(a > b && b > 5);
+    }
+}
+    
+
