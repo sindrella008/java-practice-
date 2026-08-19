29682b08aac3111529ddebe74a7c4333bee20c41
sindrella008 <sindrellasindrella37@gmail.com>
2026-08-19 22:05:45 +0530

    Create logicaloperatorspractice5.java

---
+++ logicaloperatorspractice5.java
@@ -0,0 +1,10 @@
+public class logicaloperatorprcatice5 {
+    
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 10;
+
+        System.out.println(a > 2 && b < 20);
+    }
+}
+    
