4ab5b8acc547aecbca3e5e6b967ead71f0a18acd
sindrella008 <sindrellasindrella37@gmail.com>
2026-08-19 22:11:44 +0530

    Create logicaloperatorpractice6.java

---
+++ logicaloperatorpractice6.java
@@ -0,0 +1,9 @@
+public class logicaloperatorpractice6 {
+    public class Main {
+    public static void main(String[] args) {
+        int x = 15;
+
+        System.out.println(x < 10 || x == 15);
+    }
+}
+    
