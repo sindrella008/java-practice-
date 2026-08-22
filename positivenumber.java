609bd7cf3217caa195104ee345730c8301e5dd53
sindrella008 <sindrellasindrella37@gmail.com>
2026-08-22 21:12:38 +0530

    Create positivenumber.java

---
+++ positivenumber.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+    public class positivenumber{
+    public static void main(String[]args){
+        Scanner scan =new Scanner(System.in);
+    
+        int num=scan.nextInt();
+        if(num>0){
+            System.out.println("Positive");
+        }
+
+    }
+    
+
+}
