51899ffda80eaad58d4a294a95993932425e0505
sindrella008 <sindrellasindrella37@gmail.com>
2026-08-23 22:01:07 +0530

    Create leapyear.java

---
+++ leapyear.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class LeapYear {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Enter year: ");
+        int year = scan.nextInt();
+
+        if ((year % 400 == 0) || (year % 4 == 0 && year % 100 != 0)) {
+            System.out.println("Leap Year");
+        }
+        else {
+            System.out.println("Not a Leap Year");
+        }
+    }
+}
+    
+
