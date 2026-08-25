4943f2c6b6e3671ff26e2758ad31d85951912e73
sindrella008 <sindrellasindrella37@gmail.com>
2026-08-25 22:02:56 +0530

    Implement nested loop in nestedloop.java
    Fix syntax errors and add missing components for nested loop.

---
+++ nestedloop.java
@@ -0,0 +1,13 @@
+public class NESTEDLOOP{
+    public static void main{
+        Scanner sc=new Scanner(System.in);
+        for(int i=1;i<=3;i++)
+            for(int j=1;j<=2;j++){
+        System.out.print(i+" "+j);
+        
+
+        
+
+    }
+}
+}
