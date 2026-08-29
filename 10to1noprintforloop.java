cd2e88530acfab65f74566639f4c19c62e054efc
sindrella008 <sindrellasindrella37@gmail.com>
2026-08-29 21:46:11 +0530

    Create 10to1noprintforloop.java

---
+++ 10to1noprintforloop.java
@@ -0,0 +1,10 @@
+public class tento1noprintforloop {
+    public static void main(String[]args){
+        for(int i=10;i>=1;i--){
+            System.out.println(i);
+        }
+    }
+
+    
+    
+}
